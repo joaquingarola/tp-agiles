c9d4694865de75f6d5a14198a8e8bc5a95de8592
joaquingarola <98132472+joaquingarola@users.noreply.github.com>
2023-09-11 20:50:34 -0300

    ahorcado init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { CalculadoraService } from './services/calculadora.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Clases';
-
-  constructor(private service: CalculadoraService) {}
-
-  ngOnInit(): void {
-    this.service.sumaFromString('//*\n1|3|6|4');
-  }
 }
